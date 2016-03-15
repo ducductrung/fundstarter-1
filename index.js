479dc9023412926b3d5b4532264a97246141c655
Ubuntu <ubuntu@ip-172-31-39-206.us-west-2.compute.internal>
2016-03-15 21:20:19 +0000

    assignment 1.a

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ var filename = "./index.html";
 var buf = fs.readFileSync(filename, "utf8");
 
 function start(resp) {
-    resp.writeHead(200, {"Content-type":"text/html"});
-    resp.write(buf);
-    resp.end();
-    }
+    resp.writeHead(200, {"Content-type":"text/html"});
+    resp.write(buf);
+    resp.end();
+    } 
+server.listen(8000) // for heroku use .listen(process.env.PORT || 8080)
+console.log("http://127.0.0.1:8000/")
 
-exports.start=start; 
