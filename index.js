33e1edef7fb6cc7be02e9bdd82a6c56df148d270
Ubuntu <ubuntu@ip-172-31-39-206.us-west-2.compute.internal>
2016-03-15 21:24:54 +0000

    1.b

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var fs = require("fs");
 var filename = "./index.html";
-var buf = fs.readFileSync(filename, "utf8");
 
 function start(resp) {
-    resp.writeHead(200, {"Content-type":"text/html"});
-    resp.write(buf);
-    resp.end();
-    } 
-server.listen(8000) // for heroku use .listen(process.env.PORT || 8080)
-console.log("http://127.0.0.1:8000/")
-
+    resp.writeHead(200, {
+        "Content-Type": "text/html"
+    });
+    fs.readFile(filename, "utf8", function(err, data) {
+        if (err) throw err;
+        resp.write(data);
+        resp.end();
+    });
+}
