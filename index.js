f5f3417c848e9755d7725cc70b0d20fbd7df9bf2
Ubuntu <ubuntu@ip-172-31-36-89.us-west-2.compute.internal>
2016-03-16 06:15:10 +0000

    commit1

--- index.js
+++ index.js
@@ -1,10 +1,56 @@
-var fs = require("fs");
-var filename = "./index.html";
-var buf = fs.readFileSync(filename, "utf8");
+var http = require('http')
+var fs = require('fs')
+var filename = 'index.html'
 
-function start(resp) {
-    resp.writeHead(200, {"Content-type":"text/html"});
-    resp.write(buf);
-    resp.end();
-    } 
-exports.start=start;
+var server = http.createServer(function(req,response){
+	// part 1a
+	// var buff = fs.readFileSync('index.html', "utf8")
+	// response.writeHead(200, {"Content-type":"text/html"})
+	// response.write(buff)
+	// response.end()
+	//part b
+	// response.writeHead(200, {"Content-type":"text/html"})
+	// fs.readFile(filename, "utf8", function(err,data){
+	// // body
+	// if (err) throw err
+	// response.write(data)
+	// response.end()
+	// })
+// function part2(response){
+fs.stat('index.html', function (error, stast) {
+    if (error) {
+        // Trouble accessing the file.
+        throw error;
+    }
+    // Output information about the file to the console.
+    console.log(stast);
+});
+
+response.writeHead(200, {"Content-type":"text/html"})
+fs.open('index.html','r',function(err,fd){
+
+	// Create a read buffer, and specify size
+    var readBuffer = new Buffer(2489);
+    // fs.read requires constraints on reading
+    var bufferOffset = 0,
+        bufferLength = readBuffer.length,
+        filePosition = 0;
+    fs.read(fd, 
+            readBuffer,
+            bufferOffset,
+            bufferLength,
+            filePosition,
+            function (error, readBytes) {
+                if (error) { throw error }
+                console.log("Just read " + readBytes + " bytes");
+                // Log those data to the console
+                console.log(readBuffer.slice(0, readBytes));
+               
+    			response.write(readBuffer.toString())
+    			response.end()
+            })
+    
+	})
+})
+server.listen(8080) // for heroku use .listen(process.env.PORT || 8080)
+console.log("http://127.0.0.1:8080/")
