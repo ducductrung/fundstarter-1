87e8c72dbb9e26c5ef873d2197db3a22f6832291
Ubuntu <ubuntu@ip-172-31-36-89.us-west-2.compute.internal>
2016-03-17 19:34:29 +0000

    part2

--- index.js
+++ index.js
@@ -4,12 +4,24 @@ var file = 'index.html'
 
 var server = http.createServer(function(req,response){
 	response.writeHead(200, {"Content-type":"text/html"})
-	fs.readFile(file, "utf8", function(err,data){
-		if (err) throw err
-		response.write(data)
-		response.end()
+	fs.open(file,'r',function(err,fd){
+		// Create a read buffer, and specify size
+		var readBuffer = new Buffer(2489);
+		// fs.read requires constraints on reading
+		var bufferOffset = 0,
+		bufferLength = readBuffer.length,
+		filePosition = 0;
+		fs.read(fd, readBuffer, bufferOffset, bufferLength, filePosition, function (error, readBytes)
+		{
+			if (error) { throw error }
+			console.log("Just read " + readBytes + " bytes");
+			// Log those data to the console
+			console.log(readBuffer.slice(0, readBytes));
+			response.write(readBuffer.toString())
+			response.end()
+            })
+    
 	})
 })
-server.listen(8000) // for heroku use .listen(process.env.PORT || 8000)
+server.listen(8000)
 console.log("http://127.0.0.1:8000/")
-
